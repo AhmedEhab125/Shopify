f225c7c3bea67f7a2b038ccbd5e96068b2b00ad9
milad <miladsoliman55@gmail.com>
2023-06-06 21:55:49 +0200

    Finishing Category Screen UI And Test it with Fake Data

---
+++ app/src/main/java/com/example/shopify/login/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.example.shopify.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shopify.R
+import com.example.shopify.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+  lateinit var binding:FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/com/example/shopify/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/shopify/mainActivity/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.shopify.databinding.ActivityMainBinding
 import com.example.shopify.detailsScreen.ProductDetailsFragment
 import com.example.shopify.favourite.FavouriteFragment
 import com.example.shopify.home.HomeFragment
+import com.example.shopify.login.LoginFragment
 import com.example.shopify.setting.SettingFragment
 import com.google.android.material.tabs.TabLayoutMediator
 
