3481f3ec2b18a650b1cf5040afd73cdcf87b5bc0
essam <essameldenmohamed@yahoo.com>
2023-06-06 23:01:33 +0200

    personal screen finished

---
+++ app/src/main/java/com/example/shopify/personal/view/WishListAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.shopify.personal.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shopify.R
+import com.example.shopify.databinding.FavCardBinding
+
+class WishListAdapter (var wishList: List<String>): RecyclerView.Adapter<WishListAdapter.WishViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WishViewHolder {
+        val binding = FavCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return WishViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun onBindViewHolder(holder: WishViewHolder, position: Int) {
+        holder.binding.favImg.setImageResource(R.drawable.headphone)
+        holder.binding.favProductName.text = "HeadPhone"
+        holder.binding.favProductPrice.text = "120$"
+    }
+
+    inner class WishViewHolder(var binding: FavCardBinding) : RecyclerView.ViewHolder(binding.root)
+}
