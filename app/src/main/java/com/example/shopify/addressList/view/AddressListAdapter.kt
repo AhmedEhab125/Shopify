1ef4c639332a60e60d9e2e1e399eb021887bcc98
AhmedEhab125 <121432823+AhmedEhab125@users.noreply.github.com>
2023-06-16 20:31:39 +0300

    remove Address

--- app/src/main/java/com/example/shopify/addressList/view/AddressListAdapter.kt
+++ app/src/main/java/com/example/shopify/addressList/view/AddressListAdapter.kt
@@ -1,13 +1,19 @@
 package com.example.shopify.addressList.view
 
+import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import androidx.constraintlayout.widget.Constraints
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shopify.Models.registrashonModel.Addresse
+import com.example.shopify.R
 import com.example.shopify.databinding.AdressessItemBinding
+import com.example.shopify.utiltes.LoggedUserData
 
-class AddressListAdapter(var list: MutableList<Addresse>, var iView :RemoveCustomerAddress) :
+class AddressListAdapter(var list: MutableList<Addresse>, var iView: RemoveCustomerAddress) :
     RecyclerView.Adapter<AddressListAdapter.ViewHolder>() {
     private lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,15 +34,59 @@ class AddressListAdapter(var list: MutableList<Addresse>, var iView :RemoveCusto
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
         holder.binding.tvAddress.text =
             "${list[position].address1}, ${list[position].city}, ${list[position].country}"
         holder.binding.btnDeleteAddress.setOnClickListener {
-            iView.remove(list[position].id!!)
-           list.removeAt(position)
-            notifyDataSetChanged()
+            if (list.size > 1)
+                deleteDialog(holder.binding.root.context,position)
+            else
+                cantDialog(holder.binding.root.context)
 
         }
+
+
     }
+    fun deleteDialog(context: Context,position :Int) {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.delete_address_dialog)
+        dialog.findViewById<Button>(R.id.delete).setOnClickListener {
+            iView.remove(list[position].id!!)
+            list.removeAt(position)
+            notifyDataSetChanged()
+            dialog.dismiss()
+        }
+        dialog.findViewById<Button>(R.id.cancel).setOnClickListener {
+            dialog.dismiss()
+        }
+        val window: Window? = dialog.window
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
+        window?.setLayout(
+            Constraints.LayoutParams.MATCH_PARENT,
+            Constraints.LayoutParams.WRAP_CONTENT
+        )
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.show()
+    }
+    fun cantDialog(context: Context) {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.cant_delete_address_dialog)
+
+        dialog.findViewById<Button>(R.id.cancel).setOnClickListener {
+            dialog.dismiss()
+        }
+        val window: Window? = dialog.window
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
+        window?.setLayout(
+            Constraints.LayoutParams.MATCH_PARENT,
+            Constraints.LayoutParams.WRAP_CONTENT
+        )
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.show()
+    }
+
 
     inner class ViewHolder(var binding: AdressessItemBinding) :
         RecyclerView.ViewHolder(binding.root)
