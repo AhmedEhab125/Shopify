4247569f3159a47972371828995a35a3a05e78b0
milad <miladsoliman55@gmail.com>
2023-06-17 22:38:17 +0200

    Handling UI Design

--- app/src/main/java/com/example/shopify/orderHistory/view/OrderListFragment.kt
+++ app/src/main/java/com/example/shopify/orderHistory/view/OrderListFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shopify.Models.orderList.Order
 import com.example.shopify.database.LocalDataSource
